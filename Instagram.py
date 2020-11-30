d1492733b95768776337175824c08bf8c1fe1fd6
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2020-12-01 01:59:47 +0530

    Add files via upload

--- Instagram.py
+++ Instagram.py
@@ -2,10 +2,12 @@ import praw
 
 reddit = praw.Reddit(client_id='o-0moazmFjjWNA', client_secret = 'jap7ZhKDNpKMwxlAAUWv6DUTy3Hdkg', username ='rhcpbot', password ='Reddit@9846099857', user_agent='007')
 
-subreddit = reddit.subreddit('python')
+subreddit = reddit.subreddit('kelowna')
 hot = subreddit.hot(limit=5)
 for submission in hot:
     print(submission.title)
     print(submission.ups)
     print(submission.downs)
     print(submission.visited)
+    
+subreddit.unsubscribe()
