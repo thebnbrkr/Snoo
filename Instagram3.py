5c510e067fafc5b983afe4a7be32293b6d9411fa
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2020-12-01 03:05:27 +0530

    Add files via upload

---
+++ Instagram3.py
@@ -0,0 +1,23 @@
+import praw
+
+reddit = praw.Reddit(client_id='o-0moazmFjjWNA', client_secret = 'jap7ZhKDNpKMwxlAAUWv6DUTy3Hdkg', username ='rhcpbot', password ='Reddit@9846099857', user_agent='007')
+
+subreddit = reddit.subreddit('ubco')
+hot = subreddit.hot(limit=3)
+for submission in hot:
+    print(submission.title)
+    print(submission.ups)
+    print(submission.downs)
+    print(submission.visited)
+   
+for comment in subreddit.stream.comments():
+	try:
+		parent_id = str(comment.parent())
+		
+		original = reddit.comment(parent_id)
+		print('Parent : ')
+		print(original.body)
+		print('Reply:')
+		print(comment.body)
+	except praw.exceptions.PRAWException as e:
+		pass
