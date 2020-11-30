53388c898fe6e78ab5f7444f3b176873ff72d954
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2020-12-01 03:48:28 +0530

    Add files via upload

---
+++ Instagram5.py
@@ -0,0 +1,14 @@
+import praw
+
+reddit = praw.Reddit(client_id='o-0moazmFjjWNA', client_secret = 'jap7ZhKDNpKMwxlAAUWv6DUTy3Hdkg', username ='rhcpbot', password ='Reddit@9846099857', user_agent='007')
+
+subreddit = reddit.subreddit('ubco')
+hot = subreddit.hot(limit=5)
+for submission in hot:
+    print(submission.title)
+    print(submission.ups)
+    print(submission.downs)
+    print(submission.visited)
+    
+submission = reddit.submission(url="https://www.reddit.com/r/ubco/comments/k4621y/title/")
+submission.reply('Cancun, Mexico')
