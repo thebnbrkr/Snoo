6ec55407bfd1bb6937977f7a8f392514de8ab0b8
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2020-12-02 17:05:13 +0530

    Update Instagram.py

--- Instagram.py
+++ Instagram.py
@@ -1,6 +1,6 @@
 import praw
 
-reddit = praw.Reddit(client_id='o-0moazmFjjWNA', client_secret = 'jap7ZhKDNpKMwxlAAUWv6DUTy3Hdkg', username ='rhcpbot', password ='Reddit@9846099857', user_agent='007')
+reddit = praw.Reddit(client_id='o-0moazmFjjWNA', client_secret = 'jap7ZhKDNpKMwxlAAUWv6DUTy3Hdkg', username ='rhcpbot', password ='###########', user_agent='007')
 
 subreddit = reddit.subreddit('kelowna')
 hot = subreddit.hot(limit=5)
